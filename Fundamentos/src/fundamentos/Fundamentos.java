4985c16fb636ad773c1e5a60b3592411d3e874c8
Edson Junior <edsonpetrinijr@gmail.com>
2023-07-28 10:59:04 -0300

    Declarando string como objeto

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -14,10 +14,13 @@ public class Fundamentos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Declaração variáveis de tipos primitivos
         String nome = "Moisés";
         int idade = 40;
         boolean estudando = true;
         double media = 5.5;
+        
+        String bairro = new String("Centro");
     }
     
 }
